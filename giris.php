3d2f410dd0aecef89acc95be32aad1e3c93d2733
Osman YILMAZ <osmanyilmaz299@aof.anadolu.edu.tr>
2016-04-06 11:37:01 +0300

    Update giris.php

--- giris.php
+++ giris.php
@@ -12,7 +12,7 @@ if(empty($_SESSION["dbdosya_kullanici"]) or empty($_SESSION["dbdosya_parola"]))
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="iso-8859-9">
+    <meta http-equiv="Content-Type" content="text/HTML; charset=utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="dosya yükleme">
